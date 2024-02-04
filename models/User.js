3a7241b603e567b8f616f92b94ce321af3daa2a1
joem3847 <joem3847@gmail.com>
2024-02-04 17:58:52 -0600

    Added models.

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose");
+
+// Schema for User model.
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual property `friendCount` retrieves length of user's friends array field on query.
+userSchema
+  .virtual("friendCount")
+  // Getter.
+  .get(function () {
+    return this.friends.length;
+  });
+
+// Initialize User model.
+const User = model("user", userSchema);
+
+module.exports = { User };
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const { User } = require("./User");
+const { Thought, reactionSchema } = require("./Thought");
+
+module.exports = { User, Thought, reactionSchema };
